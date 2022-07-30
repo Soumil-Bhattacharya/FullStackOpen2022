1adaf78e4479c93a5eecfcdecccc5ecdf9b8ec99
Soumil-Bhattacharya <soumilbhattacharya.sb@gmail.com>
2022-07-30 21:56:04 +0530

    part1 1.5 course information step5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,37 +1,42 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ];
+
+  const course = {
+    name :  'Half Stack application development',
+    parts: [
+      {
+        name : 'Fundamentals of React',
+        exercises : 10
+      },
+      {
+        name : 'Using props to pass data',
+        exercises : 7
+      },
+      {
+        name : 'State of a component',
+        exercises : 14
+      }
+    ]  
+  }
+
   return (
     <div>
       <div>
-        <h1>{course}</h1>
+        <h1>{course.name}</h1>
       </div>
 
       <div>
-        <p> {parts[0].name} {parts[0].exercises}  </p>
-        <p> {parts[1].name} {parts[1].exercises}  </p>
-        <p> {parts[2].name} {parts[2].exercises}  </p>
+        <p> {course.parts[0].name} {course.parts[0].exercises}  </p>
+        <p> {course.parts[1].name} {course.parts[1].exercises}  </p>
+        <p> {course.parts[2].name} {course.parts[2].exercises}  </p>
       </div>
 
       <div>
         <p>
-          Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises } 
+          Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises } 
         </p>
       </div>
     </div>
-  )
-};
+  );
+}
+
 export default App;
