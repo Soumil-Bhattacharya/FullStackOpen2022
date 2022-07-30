09112dda88949d4a9bc1df81abe3c8526c4d42c8
Soumil-Bhattacharya <soumilbhattacharya.sb@gmail.com>
2022-07-30 21:47:29 +0530

    part1 1.4 course information step4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,19 +1,19 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ];
   return (
     <div>
       <div>
@@ -21,18 +21,17 @@ const App = () => {
       </div>
 
       <div>
-        <p> {part1.name} {part1.exercises}  </p>
-        <p> {part2.name} {part2.exercises}  </p>
-        <p> {part3.name} {part3.exercises}  </p>
+        <p> {parts[0].name} {parts[0].exercises}  </p>
+        <p> {parts[1].name} {parts[1].exercises}  </p>
+        <p> {parts[2].name} {parts[2].exercises}  </p>
       </div>
 
       <div>
         <p>
-          Number of exercises { part1.exercises + part2.exercises + part3.exercises } 
+          Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises } 
         </p>
       </div>
     </div>
-  );
-}
-
+  )
+};
 export default App;
